815f0a2dce0ebebbc180d5ed7b16e255176d9fdd
ABDURAHIMAN K <laluvga@gmail.com>
2023-03-21 15:49:41 +0530

    Stage 2/5

--- stages.py
+++ stages.py
@@ -1,8 +1,21 @@
-# Stage 1/5: Unfair computer
+# Stage 2/5: Equalizing chances
+import random
+
+
 user = input()
-if user == "scissors":
-    print("Sorry, but the computer chose rock")
-elif user == "rock":
+computer = random.choice(["rock", "scissors", "paper"])
+if user == computer:
+    print(f"There is a draw ({user})")
+elif user == "rock" and computer == "scissors":
+    print("Well done. The computer chose scissors and failed")
+elif user == "rock" and computer == "paper":
     print("Sorry, but the computer chose paper")
-elif user == "paper":
+elif user == "scissors" and computer == "rock":
+    print("Sorry, but the computer chose rock")
+elif user == "scissors" and computer == "paper":
+    print("Well done. The computer chose paper and failed")
+elif user == "paper" and computer == "scissors":
     print("Sorry, but the computer chose scissors")
+elif user == "paper" and computer == "rock":
+    print("Well done. The computer chose rock and failed")
+    
