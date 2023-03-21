2cd8f4a587c99c4e4aac4a1985cd957bef242e7f
ABDURAHIMAN K <laluvga@gmail.com>
2023-03-21 15:39:34 +0530

    Stage 1/5

---
+++ stages.py
@@ -0,0 +1,8 @@
+# Stage 1/5: Unfair computer
+user = input()
+if user == "scissors":
+    print("Sorry, but the computer chose rock")
+elif user == "rock":
+    print("Sorry, but the computer chose paper")
+elif user == "paper":
+    print("Sorry, but the computer chose scissors")
